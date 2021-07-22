72ab45a67af1b824731ee2e9ff9876987b843cfd
Joshua Green <joshuagreen424@gmail.com>
2021-07-21 23:02:08 -0700

    Completed Day 3 Assignment 2

---
+++ javabasics/src/com/smoothstack/javabasics/day3/Assignment2.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package com.smoothstack.javabasics.day3;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+/**
+ * @author joshu
+ *
+ */
+public class Assignment2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String appendText = "I'm being appended!";
+		String outfile = "resources/day3/a2output.txt";
+		try {
+			Files.write(Paths.get(outfile), appendText.getBytes(), StandardOpenOption.APPEND);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
