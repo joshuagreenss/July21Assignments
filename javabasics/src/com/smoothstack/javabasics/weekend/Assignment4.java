478bfdda8270a31c881bcc622d74b1d2bd5b16a4
Joshua Green <joshuagreen424@gmail.com>
2021-07-26 04:19:48 -0700

    Completed assignment 4

--- javabasics/src/com/smoothstack/javabasics/weekend/Assignment4.java
+++ javabasics/src/com/smoothstack/javabasics/weekend/Assignment4.java
@@ -1,9 +1,23 @@
 package com.smoothstack.javabasics.weekend;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Assignment4 {
 	public List<String> noX(List<String> strings){
-		return null;
+		List<String> vals = new ArrayList<>();
+		StringBuilder clean;
+		for(String s : strings) {
+			clean = new StringBuilder();
+			for(char c : s.toCharArray()) {
+				if(c != 'x') {
+					clean.append(c);
+				}
+			}
+			if(clean.length() != 0) {
+				vals.add(clean.toString());
+			}
+		}
+		return vals;
 	}
 }
