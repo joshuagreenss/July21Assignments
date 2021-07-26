85706209839cb1d497e91123a527f3a2c2a7e67f
Joshua Green <joshuagreen424@gmail.com>
2021-07-26 02:38:12 -0700

    Completed assignment 2

--- javabasics/src/com/smoothstack/javabasics/weekend/Assignment2.java
+++ javabasics/src/com/smoothstack/javabasics/weekend/Assignment2.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 public class Assignment2 {
 	public List<Integer> getRight(List<Integer> nums){
-		
+		List<Integer> rights = List.copyOf(nums);
+		rights = rights.stream().map((i)->i%10).toList();
+		return rights;
 	}
 }
