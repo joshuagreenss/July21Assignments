64559f3564c72366f96fcd82b135f0c69fff3f69
Joshua Green <joshuagreen424@gmail.com>
2021-07-27 20:50:53 -0700

    Basic Maven project complete

---
+++ day1mavenapplication/src/main/java/day1mavenapplication/SimpleMavenProject.java
@@ -0,0 +1,19 @@
+package day1mavenapplication;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SimpleMavenProject extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		response.getWriter().append("<body>This is a basic Maven project!</body>");
+	}
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		doGet(request,response);
+	}
+}
