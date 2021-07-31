0a79461e4af7e9d00fe236bf7f94f89f240a3b48
Joshua Green <joshuagreen424@gmail.com>
2021-07-30 21:39:56 -0700

    Setting up framework and test cases

--- utopiaApplication/src/com/smoothstack/utopia/dao/DAO.java
+++ utopiaApplication/src/com/smoothstack/utopia/dao/DAO.java
@@ -5,7 +5,9 @@ package com.smoothstack.utopia.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 /**
  * @author joshu
@@ -13,20 +15,53 @@ import java.sql.SQLException;
  */
 public abstract class DAO<T> {
 	public static Connection conn = null;
-	
+
 	public DAO(Connection conn) {
 		this.conn = conn;
 	}
-	
-	public void query(String sql, Object[] vals) throws SQLException, ClassNotFoundException {
+
+	protected void commit(String sql, Object[] vals) throws SQLException, ClassNotFoundException {
 		PreparedStatement pstmt = conn.prepareStatement(sql);
 		int i = 1;
-		if(vals != null) {
-			for(Object o : vals) {
+		if (vals != null) {
+			for (Object o : vals) {
 				pstmt.setObject(i, o);
 				i++;
 			}
 		}
 		pstmt.executeUpdate();
 	}
+
+	protected Integer commitGet(String sql, Object[] vals) throws ClassNotFoundException, SQLException {
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		int i = 1;
+		if (vals != null) {
+			for (Object o : vals) {
+				pstmt.setObject(i, o);
+				i++;
+			}
+		}
+		pstmt.executeUpdate();
+		ResultSet rs = pstmt.getGeneratedKeys();
+		while (rs.next()) {
+			return rs.getInt(1);
+		}
+		return null;
+	}
+
+	protected List<T> query(String sql, Object[] vals) throws ClassNotFoundException, SQLException {
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		int i = 1;
+		if (vals != null) {
+			for (Object o : vals) {
+				pstmt.setObject(i, 0);
+				;
+				i++;
+			}
+		}
+		ResultSet rs = pstmt.executeQuery();
+		return listData(rs);
+	}
+
+	abstract protected List<T> listData(ResultSet rs) throws ClassNotFoundException, SQLException;
 }
--- utopiaApplication/src/com/smoothstack/utopia/testing/DAOTesting.java
+++ utopiaApplication/src/com/smoothstack/utopia/testing/DAOTesting.java
@@ -3,7 +3,11 @@
  */
 package com.smoothstack.utopia.testing;
 
+import java.sql.SQLException;
+
 import org.junit.Test;
+
+import com.smoothstack.utopia.dao.AirportDAO;
 
 /**
  * @author joshu
@@ -13,6 +17,11 @@ import org.junit.Test;
 public class DAOTesting {
 	@Test
 	public void airportTest() {
-		
+		AirportDAO test = new AirportDAO(null);
+		try {
+			
+		}catch(SQLException | ClassNotFoundException e) {
+			
+		}
 	}
 }
